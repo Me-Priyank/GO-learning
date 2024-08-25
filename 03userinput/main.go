cb20c0f9a6f6fe2d5984eca36b03058eeb25ec14
Me Priyank <pvonlyone29@gmail.com>
2024-08-25 16:09:41 +0530

    bufio , os , comma ok syntax learnt

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	welcome := "welcome to user input process"
 	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating of our new pizza:")
+
+	// comma ok OR err ok syntax (used to read data from another variable):-
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating", input)
+	fmt.Printf("Type of input is %T",input)
 }
