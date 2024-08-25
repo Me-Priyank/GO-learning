438a098fd42d523535f4b24e11e42e5f160140dd
Me Priyank <pvonlyone29@gmail.com>
2024-08-25 20:05:16 +0530

    handeling conversions

---
+++ 04conversion/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("welcome to pizza app")
+	fmt.Println("Rate our pizza b/w 1 to 5")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+	// fmt.Println("Rating",input)
+
+	numRate, err := strconv.ParseFloat(strings.TrimSpace(input),64)
+
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		fmt.Println("Added 1 to your rating :",numRate+1)
+	}
+}
