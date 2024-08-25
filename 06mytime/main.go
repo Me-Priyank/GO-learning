55c2d3f09711f71b15f521700e77e5145b0dcb98
Me Priyank <pvonlyone29@gmail.com>
2024-08-25 21:45:02 +0530

    learnt time handeling and its format

--- 06mytime/main.go
+++ 06mytime/main.go
@@ -1,5 +1,15 @@
-package mytime
+package main
 
-func main(){
-	
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time handeling part")
+	presentTime := time.Now()
+	fmt.Println(presentTime.Format("01-02-2006 Monday 15:04:05")) // We always have to use the exact instance as a standard to get formatted data
+
+	createDate := time.Date(2025, time.February, 10, 12, 00, 00, 00, time.UTC)
+	fmt.Println(createDate.Format("01-02-2006 Monday 15:04:05"))
 }
