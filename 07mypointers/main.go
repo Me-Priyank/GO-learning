6fd410119c2a8e9966e5c4a733a46d6a51b2c66d
Me Priyank <pvonlyone29@gmail.com>
2024-09-10 22:00:24 +0530

    .

--- 07mypointers/main.go
+++ 07mypointers/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+// Pointer gives memory referance of variable instead of variable itself, so as to avoind irregularities . because at many places , a copy of that variable is passed instead of the original one.
+
+func main() {
+	fmt.Println("Welcome to pointers")
+
+	var ptr *int 
+	fmt.Println("value is ",ptr)
 }
